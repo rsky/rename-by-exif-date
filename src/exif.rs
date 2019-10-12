447b239d3a2abeb4febf1b57a9d0bbc8d2530a65
Ryusuke SEKIYAMA <rsky0711@gmail.com>
2019-10-12 18:48:12 +0900

    Refactoring

--- src/exif.rs
+++ src/exif.rs
@@ -16,28 +16,56 @@ pub fn read_exif_date_time_original(
         Err(e) => return Err(e.to_string()),
         Ok(f) => f,
     };
+
     let reader = match Reader::new(&mut BufReader::new(&file)) {
         Err(e) => return Err(e.to_string()),
         Ok(r) => r,
     };
-    if let Some(dto) = reader.get_field(Tag::DateTimeOriginal, false) {
-        let dt_str = dto.value.display_as(dto.tag).to_string();
-        dbg!(&dt_str);
-        let naive_dt = NaiveDateTime::parse_from_str(&dt_str, "%Y-%m-%d %H:%M:%S").unwrap();
-        let utc: DateTime<Tz>;
-        if let Some(tz) = from_tz {
-            let dt = tz.from_local_datetime(&naive_dt).unwrap();
-            utc = dt.with_timezone(&UTC);
-        } else if let Some(oto) = reader.get_field(Tag::OffsetTimeOriginal, false) {
-            let dt_tz_str = format!("{}{}", dt_str, oto.value.display_as(oto.tag));
-            dbg!(&dt_tz_str);
-            let dt = DateTime::parse_from_str(&dt_tz_str, "%Y-%m-%d %H:%M:%S%:z").unwrap();
-            utc = dt.with_timezone(&UTC);
-        } else {
-            let dt = Local.from_local_datetime(&naive_dt).unwrap();
-            utc = dt.with_timezone(&UTC);
-        }
-        return Ok(Some(utc));
+
+    return Ok(read_date_time_original_as_utc(&reader, from_tz));
+}
+
+fn read_date_time_original_as_utc(reader: &Reader, from_tz: Option<Tz>) -> Option<DateTime<Tz>> {
+    let date_time_original = reader.get_field(Tag::DateTimeOriginal, false);
+    if let Some(dto) = date_time_original {
+        let offset_time_original = reader.get_field(Tag::OffsetTimeOriginal, false);
+        return Some(match (from_tz, offset_time_original) {
+            (Some(tz), _) => utc_date_time_original_with_timezone(&dto, &tz),
+            (None, Some(oto)) => utc_date_time_original_with_offset(&dto, &oto),
+            (None, None) => utc_date_time_original(&dto),
+        });
     }
-    return Ok(None);
+    return None;
+}
+
+fn field_as_string(field: &exif::Field) -> String {
+    return field.value.display_as(field.tag).to_string();
+}
+
+fn date_time_original_as_naive(dto: &exif::Field) -> NaiveDateTime {
+    let dt_str = field_as_string(&dto);
+    dbg!(&dt_str);
+    return NaiveDateTime::parse_from_str(&dt_str, "%Y-%m-%d %H:%M:%S").unwrap();
+}
+
+fn utc_date_time_original(dto: &exif::Field) -> DateTime<Tz> {
+    return Local
+        .from_local_datetime(&date_time_original_as_naive(dto))
+        .unwrap()
+        .with_timezone(&UTC);
+}
+
+fn utc_date_time_original_with_timezone(dto: &exif::Field, tz: &Tz) -> DateTime<Tz> {
+    return tz
+        .from_local_datetime(&date_time_original_as_naive(dto))
+        .unwrap()
+        .with_timezone(&UTC);
+}
+
+fn utc_date_time_original_with_offset(dto: &exif::Field, oto: &exif::Field) -> DateTime<Tz> {
+    let dt_str = format!("{}{}", field_as_string(dto), field_as_string(oto));
+    dbg!(&dt_str);
+    return DateTime::parse_from_str(&dt_str, "%Y-%m-%d %H:%M:%S%:z")
+        .unwrap()
+        .with_timezone(&UTC);
 }
